97f9631962fc8c3de49f0c4cad093b8732199823
Felipe Correia <felipedreamcorreia@hotmail.com>
2023-05-10 11:01:25 -0300

    feature: endpoint Get lists games

--- src/main/java/com/franciscogames/gameslist/dto/GameMinDTO.java
+++ src/main/java/com/franciscogames/gameslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.franciscogames.gameslist.dto;
 
 import com.franciscogames.gameslist.entities.Game;
+import com.franciscogames.gameslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -19,6 +20,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
